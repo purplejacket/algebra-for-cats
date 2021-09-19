7bf49596df985a3fc1e4204e025f69688480564f
Cristofer Herrera <crisken007@gmail.com>
2021-09-19 11:09:46 -0600

    add the basics of the game

--- GameFile1/main.js
+++ GameFile1/main.js
@@ -1,4 +1,73 @@
-add([
-    sprite('tile'),
+const SPEED = 80;
+const JUMP_FORCE = 300;
+const FALL_DEATH = 500;
+
+scene('main', () => {
+    const map = addLevel([
+        "                  ",
+        "                  ",
+        "                  ",
+        "                  ",
+        "                  ",
+        "                  ",
+        "                  ",   
+        "$$$$$$$$$         ",
+        "         $$$$$$$$$",
+        "                  ",
+        "                  ",
+    ],
+    {
+        width:20,
+        height:20,
+        "$": () => [
+            sprite("tile"),
+            area(),
+            solid()
+        ]
+        
+    })
+      
+    camScale(vec2(2,2));
     
-])
+    const player = add([
+        sprite("player"),
+        area(),
+        body(),
+        pos(20,20)
+    ]);
+    
+    player.action(() => {
+        camPos(player.pos);
+        if(player.pos.y > FALL_DEATH){
+            go("gameover")
+        }
+    })
+    
+    keyDown("right", () => {
+        player.move(SPEED, 0)
+    })
+    
+    keyDown("left", () => {
+        player.move(-SPEED, 0)
+    })
+    
+    keyPress("space", () => {
+        if(player.grounded()){
+            player.jump(JUMP_FORCE);
+        }
+    })
+})
+
+scene("gameover", () => {
+    add([
+        text("you died", 50),
+        color(1,1,1),
+        origin("center"),
+        pos(width()/2, height()/2)
+    ])
+    keyPress("space", () => {
+        go("main")
+    })
+})
+
+go("main")
